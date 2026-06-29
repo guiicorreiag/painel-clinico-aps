e2268330f741412775edcf33f8e944487670283e
guiicorreiag <guilhermec.guimaraes@hotmail.com>
2026-06-28 23:46:11 -0300

    Add files via upload
    Atualiza PWA para v0.28 Receita Expandida

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-clinico-aps-v0-17-pwa';
+const CACHE_NAME = 'painel-clinico-aps-v0-28-receita-expandida-r2';
 const APP_SHELL = [
   './',
   './index.html',
@@ -12,7 +12,7 @@ const APP_SHELL = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+      .then(cache => cache.addAll(APP_SHELL).catch(() => cache.addAll(['./', './index.html'])))
       .then(() => self.skipWaiting())
   );
 });
@@ -28,13 +28,12 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
+    fetch(event.request)
+      .then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
         return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
   );
 });
